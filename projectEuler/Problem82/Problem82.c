99e503088d31194da24a2393db1e584b52672040
Robert Blacha <robertpblacha@gmail.com>
2024-10-01 14:16:37 -0400

    Finished first A* and started second

--- projectEuler/Problem82/Problem82.c
+++ projectEuler/Problem82/Problem82.c
@@ -7,14 +7,12 @@ unsigned long average = 0; //For predicting path from a certain point, assume ev
 typedef struct node {
 	unsigned int value;
 	unsigned int position;
-	unsigned long cost;
 	struct node* parent;
 	struct node* next;
 } node;
 
-unsigned int predict(unsigned int pos) {
-	unsigned int cursorX = pos % MATSIZE, cursorY = pos / MATSIZE;
-	return ((MATSIZE-1)-cursorX + (MATSIZE-1)-cursorY) * average; // number of steps left times average
+unsigned int predict(unsigned int cursorX, unsigned int cursorY) {
+	return ((MATSIZE-1)-cursorX) * average; // number of steps left plus average
 }
 
 int inside(unsigned int position, node *list) {
@@ -30,14 +28,10 @@ int inside(unsigned int position, node *list) {
 node *addToList(unsigned int position, unsigned int *matrix, node *list, node* parent) {
 	node * insert;
 	if(!list->value) { //first or last node in linked list
-		if(parent) {
-			list->value = matrix[position] + parent->cost + predict(position);
-			list->cost = matrix[position] + parent->cost;
-		}
-		else {
-			list->value = matrix[position] + predict(position);
-			list->cost = matrix[position];
-		}
+		if(parent)
+			list->value = matrix[position] + parent->value;
+		else
+			list->value = matrix[position];
 		list->position = position;
 		list->next = calloc(1, sizeof(node));
 		list->parent = parent;
@@ -46,14 +40,13 @@ node *addToList(unsigned int position, unsigned int *matrix, node *list, node* p
 	if(list->position == position) { //node already in linkedList
 		return NULL;
 	}
-	if(list->value > matrix[position]+parent->cost+predict(position)) { //insert in middle of linkedList
+	if(list->value > matrix[position]+parent->value) { //insert in middle of linkedList
 		insert = calloc(1, sizeof(node));
 		memcpy(insert, list, sizeof(node));
-		list->value = matrix[position] + parent->cost + predict(position);
+		list->value = matrix[position] + parent->value;
 		list->position = position;
 		list->next = insert;
 		list->parent = parent;
-		list->cost = matrix[position] + parent->cost;
 		return list;
 	}
 	addToList(position, matrix, list->next, parent);
@@ -73,14 +66,16 @@ unsigned long aStarSearch(unsigned int *matrix) {
 	node *open = calloc(1, sizeof(node));
 	node *closed = calloc(1, sizeof(node));
 	node *intermediary = NULL; // Just used as like a scratch space
-	addToList(0, matrix, open, NULL);
+	for(int i = 0; i < MATSIZE; i++)
+		addToList(i*MATSIZE, matrix, open, NULL);
+	printf("AAAA\n");
 	unsigned int pos;
 	while(open->value) {
 		cursorX = open->position % MATSIZE;
 		cursorY = open->position / MATSIZE;
-		if(cursorX == MATSIZE-1 && cursorY == MATSIZE-1) {//solution found
-			addToList((MATSIZE-1)*(MATSIZE-1), matrix, open, open);
-			Path = open->cost;
+		if(cursorX == MATSIZE-1) {//solution found
+			intermediary = addToList((MATSIZE-1)*(MATSIZE-1), matrix, open, open);
+			Path = sum(open, matrix);
 			while(intermediary=open) {
 				open = open->next;
 				free(intermediary);
@@ -95,12 +90,15 @@ unsigned long aStarSearch(unsigned int *matrix) {
 		open = open->next;
 		intermediary->next = closed;
 		closed = intermediary;
+
 		pos = cursorX+1 + MATSIZE * cursorY;
-		
 		if(!inside(pos, closed) && cursorX < MATSIZE-1)
 			addToList(pos, matrix, open, intermediary);
 		pos = cursorX + MATSIZE * (cursorY+1);
 		if(!inside(pos, closed) && cursorY < MATSIZE-1) 
+			addToList(pos, matrix, open, intermediary);
+		pos = cursorX + MATSIZE * (cursorY-1);
+		if(!inside(pos, closed) && cursorY >= 0) 
 			addToList(pos, matrix, open, intermediary);
 	}
 }
@@ -110,11 +108,9 @@ unsigned int *initArray() {
 	unsigned int *matrix = malloc(sizeof(unsigned int) * MATSIZE * MATSIZE);
 	for(unsigned int i = 0; i < MATSIZE*MATSIZE; i++) {
 		fscanf(mat, "%u, ", &matrix[i]);
-		if(matrix[i] > average) {
-			average += matrix[i];
-		}
+		average += matrix[i];
 	}
-	average /= MATSIZE;
+	average /= MATSIZE*MATSIZE;
 	return matrix;
 }
 
